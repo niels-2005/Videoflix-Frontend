0f04c0e70dee2b985712d6334b66c905c5b7a389
niels-2005 <niels.scholz01@web.de>
2023-07-05 11:37:44 +0200

    added login/sign-up/forgot-password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NeedhelpComponent } from './components/needhelp/needhelp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent,
+    NeedhelpComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  switchContainer(id1: string, id2: string, id3: string, id4: string){
+    document.getElementById(id1)?.classList.add('d-none')
+    document.getElementById(id2)?.classList.add('d-none');
+    document.getElementById(id3)?.classList.remove('d-none');
+    document.getElementById(id4)?.classList.remove('d-none');
+  }
+
 }
